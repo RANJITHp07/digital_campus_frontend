7e6263176219a5e147cd917f9c6bb8fc862edc03
ranjith <ranjithp27122003@gmail.com>
2023-12-31 11:05:58 +0530

    fix:added header into the interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,14 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const Api = axios.create({ baseURL:"https://www.digitalcampus.shop" });
+const cookies = Cookies.get('accessToken');
+console.log(cookies);
+
+const Api = axios.create({
+    baseURL: "https://www.digitalcampus.shop",
+    headers: {
+        Authorization: cookies || ''
+    }
+});
 
 export default Api;
