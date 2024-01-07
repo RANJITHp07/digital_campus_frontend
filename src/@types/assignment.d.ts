bb5b755bfca85a6e2239d25c612b225f686dc4ec
ranjith <ranjithp27122003@gmail.com>
2024-01-07 11:21:45 +0530

    reafctor:reorganised the @types folder

--- src/@types/assignment.d.ts
+++ src/@types/assignment.d.ts
@@ -1,41 +1,39 @@
-export interface Question{
-    type:string,
-    question:string,
-    answers:string[],
-    realAnswers:any[]
-    edit:boolean
+export interface Question {
+  type: string;
+  question: string;
+  answers: string[];
+  realAnswers: any[];
+  edit: boolean;
 }
 
-
-export interface Event{
-    title:string,
-    dueDate:{
-        day:string,
-        time:string
-    },
-    creator:string,
-    class_id:string[]
+export interface Event {
+  title: string;
+  dueDate: {
+    day: string;
+    time: string;
+  };
+  creator: string;
+  class_id: string[];
 }
 
-export interface Polling{
-    __typename:string,
-    title:string,
-    polling:{
-        __typename?:string,
-        answers:string[]
-    },
-    students:string[]
-}
-
-export interface Comment{
-    username:string,
-    comment:string
-}
-
-export interface Quiz{
-    question: string;
+export interface Polling {
+  __typename: string;
+  title: string;
+  polling: {
+    __typename?: string;
     answers: string[];
-    type: string;
-    realAnswers:string[]
+  };
+  students: string[];
 }
 
+export interface Comment {
+  username: string;
+  comment: string;
+}
+
+export interface Quiz {
+  question: string;
+  answers: string[];
+  type: string;
+  realAnswers: string[];
+}
--- src/@types/classroom.d.ts
+++ src/@types/classroom.d.ts
@@ -1,35 +1,35 @@
-export interface ClassroomProps{
-    _id?:string,
-    className?:string,
-    classSection?:string,
-    classSubject?:string,
-    creator?:string,
-    students_enrolled?:string[],
-    admins?:string[],
-    classCode?:string,
-    backgroundPicture?:string,
-    createdAt?:Date
-    category?:string,
-    block?:boolean
-    profile?:string
-    blockClassroom?:boolean
-    themeColor?:string
+export interface ClassroomProps {
+  _id?: string;
+  className?: string;
+  classSection?: string;
+  classSubject?: string;
+  creator?: string;
+  students_enrolled?: string[];
+  admins?: string[];
+  classCode?: string;
+  backgroundPicture?: string;
+  createdAt?: Date;
+  category?: string;
+  block?: boolean;
+  profile?: string;
+  blockClassroom?: boolean;
+  themeColor?: string;
 }
 
 export interface ClassProps {
-    className: string;
-    creator: string;
-    id: string;
-    code?: string;
-    type: boolean;
-    bg: string;
-    subject?: string;
-    section?: string;
-    profile: string;
-    block?: boolean;
-  }
-
-export type Polling={
-    number:number,
-    question:string
+  className: string;
+  creator: string;
+  id: string;
+  code?: string;
+  type: boolean;
+  bg: string;
+  subject?: string;
+  section?: string;
+  profile: string;
+  block?: boolean;
 }
+
+export type Polling = {
+  number: number;
+  question: string;
+};
